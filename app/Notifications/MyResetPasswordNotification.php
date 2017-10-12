a898673c1c442ea94f02e3d2ce023447f2025722
wallacecarvaalho <wallace.c.aleixo00@gmail.com>
2017-10-12 00:33:02 -0300

    Alteração layout envio de email de recuperacao de senha

--- app/Notifications/MyResetPasswordNotification.php
+++ app/Notifications/MyResetPasswordNotification.php
@@ -2,9 +2,7 @@
 
 namespace App\Notifications;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Notification;
-use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Notifications\Messages\MailMessage;
 
 class MyResetPasswordNotification extends ResetPassword
@@ -13,6 +11,7 @@ class MyResetPasswordNotification extends ResetPassword
     public function toMail($notifiable)
     {
         return (new MailMessage)
+            ->greeting('Olá')
             ->line('Voce Esta recebendo esse email para redefinir a sua senha')
             ->action('Redefinir senha', url(config('app.url').route('password.reset', $this->token, false)));
     }
---
+++ resources/views/vendor/notifications/email.blade.php
@@ -0,0 +1,57 @@
+@component('mail::message')
+{{-- Greeting --}}
+@if (! empty($greeting))
+# {{ $greeting }}
+@else
+@if ($level == 'error')
+# Whoops!
+@else
+# Hello!
+@endif
+@endif
+
+{{-- Intro Lines --}}
+@foreach ($introLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+    switch ($level) {
+        case 'success':
+            $color = 'green';
+            break;
+        case 'error':
+            $color = 'red';
+            break;
+        default:
+            $color = 'blue';
+    }
+?>
+@component('mail::button', ['url' => $actionUrl, 'color' => $color])
+{{ $actionText }}
+@endcomponent
+@endisset
+
+{{-- Outro Lines --}}
+@foreach ($outroLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Salutation --}}
+@if (! empty($salutation))
+{{ $salutation }}
+@else
+EPet,<br>
+@endif
+
+{{-- Subcopy --}}
+@isset($actionText)
+@component('mail::subcopy')
+Ou copie e cole a Url em seu navegador: [{{ $actionUrl }}]({{ $actionUrl }})
+@endcomponent
+@endisset
+@endcomponent
