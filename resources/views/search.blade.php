c18e33b0aa3d638b4ecbed437273c097a6672c42
Matheus <matheuspq79@outlook.com.br>
2017-11-25 17:10:30 -0200

    Correção de erros na tela de Search. Adicionado tbm cor na borda das tabelas

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -6,7 +6,7 @@
         <div class="container fundo">
             <!--Coluna de categorias-->
             <section class="categorias">
-                <div class="col-md-3 col-categoria">
+                {{--  <div class="col-md-3 col-categoria">
 
                     <h3>Categorias</h3>
                      <ul>
@@ -15,12 +15,12 @@
                         <li class="lista-categorias"><a href="">Papagaio</a></li>
                     </ul>
 
-                </div>
+                </div>  --}}
             </section>
             <!--Coluna de produtos-->
             <section class="produtos">
             
-                <div class="col-md-9 col-produto">
+                <div class="col-md-12 col-produto">
                     <h3>Produtos</h3>
                         <div class="row">
                         @if(empty($produtos_pesq) || $produtos_pesq == '[]')
@@ -29,14 +29,22 @@
                         </div>
                         @else
                             @foreach($produtos_pesq as $p)
-                                <div class="col-sm-6 col-md-4 col-itens">
-                                    <a href="/produtos/{{$p->produto_id}}>">
+                                <div class="col-sm-6 col-md-3 col-itens">
+                                    <a href="/produtos/{{$p->id}}>">
                                         <div class="thumbnail">
                                             <img src="{{$p->imagem}}" class="img-rounded" alt="...">
                                             <div class="caption">
                                                 <h3>{{$p->name}}</h3>
                                                 <p class="preco">R$ {{$p->preco}}</p>
-                                                <p><a href="#" class="btn btn-success btn-comprar" role="button">Comprar</a></p>
+
+                                                <form method="POST" action="{{ route('carrinho.adicionar') }}">
+                                                    
+                                                    {{ csrf_field() }}
+                                                    <input type="hidden" name="id" value="{{ $p->id }}">
+                                                    <button class="btn btn-success btn-comprar">Comprar</button>
+                                                    {{--  <p><a href="#" class="btn btn-success btn-comprar" role="button">Comprar</a></p>  --}}
+                                                </form>
+                                                {{--  <p><a href="#" class="btn btn-success btn-comprar" role="button">Comprar</a></p>  --}}
                                             </div>
                                         </div>
                                     </a>
